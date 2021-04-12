b9b5f58b7ac2b5a88d2364ce41cb0316740fd498
julieconnors <juliana.g.connors@gmail.com>
2021-04-12 08:13:05 -0400

    create store and set up dev tools and thunk

--- src/index.js
+++ src/index.js
@@ -2,12 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
+import horseReducer from './reducers/horseReducer'
 import App from './App';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(horseReducer, 
+  composeEnhancers(
+    applyMiddleware(thunk)
+    )
+);
+
 ReactDOM.render(
-    <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
