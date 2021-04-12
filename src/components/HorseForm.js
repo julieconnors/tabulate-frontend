cc138e93683c622ff6040f3e990cd8dc7e94f42b
julieconnors <juliana.g.connors@gmail.com>
2021-04-12 08:09:11 -0400

    create horse and ride form components

---
+++ src/components/HorseForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class HorseForm extends Component {
+    render() {
+        return (
+            <div>Horse</div>
+        )
+    }
+}
+
+export default HorseForm;
