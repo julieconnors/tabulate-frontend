e4237c97081df2123e7ce1206517b1d1ca601958
julieconnors <juliana.g.connors@gmail.com>
2021-04-13 11:55:33 -0400

    add addRide function

---
+++ src/actions/addRide.js
@@ -0,0 +1,15 @@
+export const addRide = (data) => {
+
+    return (dispatch) => {
+        fetch('http://localhost:5500/api/v1/rides', {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }, 
+            method: 'POST',
+            body: JSON.stringify(data)
+        })
+        // .then(res => res.json())
+        // .then(ride => dispatch({type: 'ADD_RIDE'}))
+    }
+}
