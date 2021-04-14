be74475afff758bd925b1454423f524b08bb1e88
julieconnors <juliana.g.connors@gmail.com>
2021-04-14 06:41:04 -0400

    remove fetch from component

--- src/components/Rides.js
+++ src/components/Rides.js
@@ -1,12 +1,5 @@
 import React, { Component } from 'react';
-import { fetchRides } from '../actions/fetchRides';
-import { connect } from 'react-redux';
-
 class Rides extends Component {
-
-    componentDidMount() {
-        this.props.fetchRides()
-    }
 
     // filterRides() {
     //     this.props.rides.filter(ride => ride.attributes.date === this.props.date)
@@ -14,14 +7,14 @@ class Rides extends Component {
 
     render() {
         const filteredRides = this.props.rides.filter(ride => ride.attributes.date === this.props.date)
+
         const rideList = filteredRides.map(ride => {
             return (
                 <tr key={ride.id}>
-                    <td>{ride.attributes.horse_id}</td>
-                    <td>training option</td>
+                    <td>{ride.attributes.horse.name}</td>
+                    <td>{ride.attributes.training_option.label}</td>
                 </tr>)
     })
-
         return (
             <div>
                 <table>
@@ -40,10 +33,4 @@ class Rides extends Component {
     }
 }
 
-const mapStateToProps = state => {
-    return {
-        rides: state.rides
-    }
-}
-
-export default connect(mapStateToProps, { fetchRides })(Rides);
+export default Rides;
