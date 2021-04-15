ca7a0805d0f345e9f083bf01425b1e3ef5c49304
julieconnors <juliana.g.connors@gmail.com>
2021-04-15 06:50:44 -0400

    fix autofill bug so last then has dispatch

--- src/actions/fetchOwners.js
+++ src/actions/fetchOwners.js
@@ -1,8 +1,11 @@
 export function fetchOwners() {
-    fetch('http://localhost:5500/api/v1/owners')
-    .then(res => res.json())
-    .then(data => dispatchEvent({
-        type: 'FETCH_OWNERS',
-        payload: data
-    }))
+    return (dispatch) => {
+        fetch('http://localhost:5500/api/v1/owners')
+        .then(res => res.json())
+        .then(data => dispatch({
+            type: 'FETCH_OWNERS',
+            payload: data
+        }))
+    }
+    
 }
