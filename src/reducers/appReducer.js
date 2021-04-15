e33c78658229d93ff659867b36cda26a8c2bcbac
julieconnors <juliana.g.connors@gmail.com>
2021-04-14 20:13:32 -0400

    create general app reducer

---
+++ src/reducers/appReducer.js
@@ -0,0 +1,25 @@
+export default function appReducer(state = {
+    horses: [],
+    trainingOptions: [],
+    rides: [],
+    owners: []
+}, action) {
+    switch (action.type) {
+        case 'FETCH_HORSES':
+            return { ...state, horses: action.payload.data}
+
+        case 'FETCH_RIDES':
+            return {...state, rides: action.payload.data}
+
+        case 'ADD_RIDE':
+            return { ...state, rides: [...state.rides, action.payload.data] }
+
+            case 'FETCH_TRAINING_OPTIONS':
+            return { ...state, trainingOptions: action.payload.data } 
+
+            
+
+        default:
+            return state
+    }
+}
