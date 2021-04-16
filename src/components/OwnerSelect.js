4aeddede4184a9fb8d08a45d680a15694b777d2e
julieconnors <juliana.g.connors@gmail.com>
2021-04-16 06:48:29 -0400

    remove date input and import calendar component

--- src/components/OwnerSelect.js
+++ src/components/OwnerSelect.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
-
+import Calendar from './Calendar'
 class OwnerSelect extends Component {
 
     state = {
-        owner_id: "",
-        start_date: "",
-        end_date: ""
+        owner_id: ""
     }   
+
+    handleSelect(){
+
+      }
+
+    handleSubmit = () => {
+
+    }
 
     render() {
         const ownerOptions = this.props.owners.map(owner => <option key={owner.id} value={owner.id}>{owner.attributes.name}</option>)
+
         return (
             <div>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                 <label>Owner:</label>
                     <select name="owners">
                         {ownerOptions}
                     </select>
-                    <input type="date"/>
+                    <Calendar />
+                    {/* <input type="date"/> */}
                 </form>
             </div>
         )
