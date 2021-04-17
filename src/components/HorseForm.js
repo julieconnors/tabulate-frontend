f7ea0a3f3d171f7087430ae6a7fa0b2567701c35
julieconnors <juliana.g.connors@gmail.com>
2021-04-16 20:58:38 -0400

    add form to component

--- src/components/HorseForm.js
+++ src/components/HorseForm.js
@@ -2,11 +2,36 @@ import React, { Component } from 'react';
 
 class HorseForm extends Component {
 
+    state = {
+        name: "",
+        owner: ""
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        // dispatch action to add horse
+        this.props.addHorse(this.state.name)
+        this.setState({
+            name: ""
+        })
+    }
 
     render() {
         return (
             <div>
-                Horse
+                <form onSubmit={this.handleSubmit}>
+                    <label>Horse Name</label>
+                    <input type="text" value={this.state.name} onChange={this.handleChange}/><br></br>
+                    <label>Owner Name</label>
+                    <input type="text" value={this.state.owner} onChange={this.handleChange}/><br></br>
+                    <input type="submit" value="Add Horse"/>
+                </form>
             </div>
         )
     }
