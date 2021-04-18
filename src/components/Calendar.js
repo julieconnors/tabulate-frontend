b5b8c0c145ea025bde259d24f83debaf693e923a
julieconnors <juliana.g.connors@gmail.com>
2021-04-17 21:01:13 -0400

    add component to handle date range selection

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -9,18 +9,34 @@ import 'react-date-range/dist/theme/default.css'; // theme css file
 // import { Calendar } from 'react-date-range';
 // import { DateRange } from 'react-date-range';
 
-function Calendar() {
-
+function Calendar(props) {
+    
     const [state, setState] = useState([
         {
           startDate: new Date(),
-          endDate: addDays(new Date(), 7),
+          endDate: addDays(new Date(), 0),
           key: 'selection'
         }
       ]);
-      return (
-        <DateRangePicker ranges={state} onChange={item => setState([item.selection])} showSelectionPreview={true}/>
 
+      // const handleOnSubmit = () => {
+      //   let data = [...state]
+      //   // debugger
+
+      //   props.collectDateRange(data)
+      // }
+      // on submit, pass date ranges from local state to parent component
+
+      return (
+        <div>
+          <form >
+            <DateRangePicker ranges={state} onChange={item => {
+            setState([item.selection])
+            props.collectDateRange(state)
+            }} showSelectionPreview={true}/><br></br>
+            <input type="submit" value="Select Dates"/>
+          </form>
+        </div>
       )
 
 
