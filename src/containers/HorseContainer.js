3a216bb0bd1adcef36950f30dd99f2be04619fa5
julieconnors <juliana.g.connors@gmail.com>
2021-04-19 13:03:43 -0400

    create horse container to fetch horses for home page

---
+++ src/containers/HorseContainer.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Horses from '../components/horses/Horses'
+import HorseForm from '../components/horses/HorseForm'
+import { connect } from 'react-redux'
+import { fetchHorses } from '../actions/fetchHorses'
+
+class HorseContainer extends Component {
+
+    componentDidMount() {
+        this.props.fetchHorses()
+    }
+
+    render() {
+        return (
+        <div>
+            <Horses horses={this.props.horses}/>
+            <HorseForm/>
+        </div>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        horses: state.horses
+    }
+}
+
+export default connect(mapStateToProps, { fetchHorses })(HorseContainer);
