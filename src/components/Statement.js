40453cd24d78fea4f74a79c4b9003d344d6f9943
julieconnors <juliana.g.connors@gmail.com>
2021-04-19 05:56:08 -0400

    create Statement component to display data

---
+++ src/components/Statement.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Statement extends Component {
+    render() {
+        
+        return (
+            <div>Statement</div>
+        )
+    }
+}
+
+export default Statement;
