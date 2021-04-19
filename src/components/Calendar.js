fc7ae8687a3ab94ee5d8e16c33407144477547b3
julieconnors <juliana.g.connors@gmail.com>
2021-04-19 05:59:58 -0400

    include state change so calendar show selected dates

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,45 +1,39 @@
 import { DateRangePicker } from 'react-date-range';
-import { addDays } from 'date-fns';
-import { useState } from 'react';
+import React, { Component } from 'react';
 
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
+ 
+class Calendar extends Component {
 
+  state = {
+    selectionRange : {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection"
+    }
+  }
 
-// import { Calendar } from 'react-date-range';
-// import { DateRange } from 'react-date-range';
-
-function Calendar(props) {
+  handleSelect = (ranges) => {
+    console.log(ranges.selection);
     
-    const [state, setState] = useState([
-        {
-          startDate: new Date(),
-          endDate: addDays(new Date(), 0),
-          key: 'selection'
-        }
-      ]);
+    this.setState({
+      selectionRange : {
+        startDate: ranges.selection.startDate,
+        endDate: ranges.selection.endDate,
+        key: "selection"
+      }
+    })
 
-      // const handleOnSubmit = () => {
-      //   let data = [...state]
-      //   // debugger
-
-      //   props.collectDateRange(data)
-      // }
-      // on submit, pass date ranges from local state to parent component
-
-      return (
-        <div>
-          <form >
-            <DateRangePicker ranges={state} onChange={item => {
-            setState([item.selection])
-            props.collectDateRange(state)
-            }} showSelectionPreview={true}/><br></br>
-            <input type="submit" value="Select Dates"/>
-          </form>
-        </div>
-      )
-
-
+    //pass ranges back to parent
+    this.props.collectDateRange(ranges.selection)    
+  }
+    
+  render(){
+    return (
+      <DateRangePicker ranges={[this.state.selectionRange]} onChange={this.handleSelect} />
+    )
+  }
 }
 
 export default Calendar;
