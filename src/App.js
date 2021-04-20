5de8e6365839293a25ad39cb16ecf8a0b1c74439
julieconnors <juliana.g.connors@gmail.com>
2021-04-20 18:38:45 -0400

    import bootstrap components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 import './App.css';
+
 import React, { Component } from 'react';
 import HorseContainer from './containers/HorseContainer';
 import TrainingOptionsContainer from './containers/TrainingOptionsContainer';
@@ -9,6 +15,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        
         <HorseContainer />
         <TrainingOptionsContainer />
         {/* <ErrorBoundary> */}
