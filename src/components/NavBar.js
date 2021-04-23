0258777342f3d87be64e5244a05e9b1eac955802
julieconnors <juliana.g.connors@gmail.com>
2021-04-22 21:44:57 -0400

    import styled and add declarations for nav links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,16 @@ import Navbar from 'react-bootstrap/Navbar'
 // import Nav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/Button'
+import styled from '@emotion/styled'
 
+const MenuDiv = styled.div`
+    .nav {
+        color: black;
+        }
+    Button {
+        margin-right: 10px;
+        }
+`
 
 
 class NavBar extends Component {
@@ -12,9 +21,11 @@ class NavBar extends Component {
         return (
             <Container>
                 <Navbar>
-                    <Button variant="info"><NavLink to="/" className="nav">Home</NavLink></Button>
-                    <Button variant="light"><NavLink to="/day-sheet" className="nav">Day Sheet</NavLink></Button>
-                    <Button variant="light"><NavLink to="/statements" className="nav">Statements</NavLink></Button>
+                    <MenuDiv>
+                        <Button variant="info"><NavLink to="/" className="nav">Home</NavLink></Button>
+                        <Button variant="light"><NavLink to="/day-sheet" className="nav">Day Sheet</NavLink></Button>
+                        <Button variant="light"><NavLink to="/statements" className="nav">Statements</NavLink></Button>
+                    </MenuDiv>
                 </Navbar>
             </Container>
         )
