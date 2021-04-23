63c3906481296cd3ef8f62b0e5290923986ed7fb
julieconnors <juliana.g.connors@gmail.com>
2021-04-22 21:47:32 -0400

    update props

--- src/containers/OwnerStatementContainer.js
+++ src/containers/OwnerStatementContainer.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { fetchOwners } from '../actions/fetchOwners';
 import { fetchRides } from '../actions/fetchRides'
+import { fetchTrainingOptions } from '../actions/fetchTrainingOptions'
+
 import { connect } from 'react-redux';
 import OwnerStatementForm from '../components/OwnerStatementForm';
 class OwnerStatementContainer extends Component {
@@ -8,12 +10,13 @@ class OwnerStatementContainer extends Component {
     componentDidMount() {
         this.props.fetchOwners()
         this.props.fetchRides()
+        this.props.fetchTrainingOptions()
     }
     
     render() {
         return (
             <div>
-                <OwnerStatementForm owners={this.props.owners} rides={this.props.rides}/>
+                <OwnerStatementForm owners={this.props.owners} rides={this.props.rides} trainingOptions={this.props.trainingOptions}/>
             </div>
         )
     }
@@ -22,8 +25,9 @@ class OwnerStatementContainer extends Component {
 const mapStateToProps = state => {
     return {
         owners: state.owners,
-        rides: state.rides
+        rides: state.rides,
+        trainingOptions: state.trainingOptions
     }
 }
 
-export default connect(mapStateToProps, { fetchOwners, fetchRides })(OwnerStatementContainer);
+export default connect(mapStateToProps, { fetchOwners, fetchRides, fetchTrainingOptions })(OwnerStatementContainer);
