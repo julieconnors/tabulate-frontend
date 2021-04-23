3d8f5a0ec34cb1991607277ebe1f58b5b04791b5
julieconnors <juliana.g.connors@gmail.com>
2021-04-22 21:52:16 -0400

    include css for date range

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,5 +1,8 @@
 import { DateRangePicker } from 'react-date-range';
 import React, { Component } from 'react';
+
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css';
 class Calendar extends Component {
 
   state = {
