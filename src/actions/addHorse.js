04fee970fd6f03a1ad50ebb9a3b9e7ef4fbe7966
julieconnors <juliana.g.connors@gmail.com>
2021-04-26 14:44:42 -0400

    update port to 3001

--- src/actions/addHorse.js
+++ src/actions/addHorse.js
@@ -1,7 +1,6 @@
-export const addHorse = (data) => {
-
+export function addHorse(data) {
     return (dispatch) => {
-        fetch('http://localhost:5500/api/v1/horses', {
+        fetch('http://localhost:3001/api/v1/horses', {
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json'
