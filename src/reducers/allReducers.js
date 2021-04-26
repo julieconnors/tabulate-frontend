32d1d5016eed2bf0c5da3f98690dbd3a2455eb0b
julieconnors <juliana.g.connors@gmail.com>
2021-04-26 14:37:23 -0400

    import all reducer files to create combined reducer

--- src/reducers/allReducers.js
+++ src/reducers/allReducers.js
@@ -1,50 +1,14 @@
 import { combineReducers } from 'redux';
+import horseReducer from './horseReducer'
+import serviceReducer from './serviceReducer'
+import rideReducer from './rideReducer'
+import ownerReducer from './ownerReducer'
 
 const allReducers = combineReducers({
     horses: horseReducer,
     rides: rideReducer,
-    trainingOptions: trainingOptionReducer,
+    services: serviceReducer,
     owners: ownerReducer
 })
 
 export default allReducers;
-
-function horseReducer(state = [], action) {
-    switch (action.type) {
-        case 'FETCH_HORSES':
-            return [...state, action.payload.data]
-
-        default:
-            return state
-    }
-}
-
-function rideReducer(state = [], action) {
-    switch (action.type) {
-        case 'FETCH_RIDES':
-            return [...state, action.payload.data]
-
-        case 'ADD_RIDE':
-            return { ...state, rides: [...state.rides, action.payload] }
-
-        default:
-            return state
-    }
-}
-
-function trainingOptionReducer(state = [], action) {
-    switch(action.type) {
-        case 'FETCH_TRAINING_OPTIONS':
-            return [...state, action.payload.data] 
-
-        default:
-            return state
-    }
-}
-
-function ownerReducer(state = [], action) {
-    switch(action.type) {
-        default:
-            return state
-    }
-}
