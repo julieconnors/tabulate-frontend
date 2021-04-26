a1c60d8dd7aa4971df90c0ee2ea1ea28f8f31364
julieconnors <juliana.g.connors@gmail.com>
2021-04-26 14:35:04 -0400

    add folder for ride components

---
+++ src/components/rides/Rides.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import styled from '@emotion/styled'
+
+const TodaysRides = styled.div`;
+    background-color: #2F5061;
+    color: #F4EAE6;
+    text-align: center;
+    margin: 5%;
+    padding: 2%;
+    .table {
+        width: 50%;
+        margin-left: 25%;
+    }
+`
+class Rides extends Component {
+
+    render() {
+        const filteredRides = this.props.rides.filter(ride => ride.attributes.date === this.props.date)
+
+        const rideList = filteredRides.map(ride => {
+            return (
+                <div className="tr" key={ride.id}>
+                    <span className='td'>{ride.attributes.horse.name}</span>
+                    <span className='td'>{ride.attributes.service.label}</span>
+                </div>)
+    })
+        return (
+            <TodaysRides>
+                <h4>Today</h4>
+                <div className='table'>
+                        {rideList}
+                </div>
+            </TodaysRides>
+        )
+    }
+}
+
+export default Rides;
