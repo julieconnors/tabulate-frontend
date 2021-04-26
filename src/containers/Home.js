5cd27c13dce03f7bc83c6bccaf9f8d2b44c42906
julieconnors <juliana.g.connors@gmail.com>
2021-04-26 14:33:11 -0400

    create Home container

---
+++ src/containers/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import HorseContainer from './HorseContainer';
+import ServicesContainer from './ServicesContainer';
+// import ErrorBoundary from './components/ErrorBoundary'
+
+class Home extends Component {
+
+    render() {
+        return (
+            <div>
+                {/* <ErrorBoundary> */}
+                    <HorseContainer />
+                    <ServicesContainer />
+                {/* </ErrorBoundary> */}
+            </div>
+        )
+    }
+
+}
+
+export default Home;
