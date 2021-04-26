b9d8cd9af1f31b248a95943f933cd2ede54e42f8
julieconnors <juliana.g.connors@gmail.com>
2021-04-26 14:34:32 -0400

    update container to services instead of trainging options

---
+++ src/containers/ServicesContainer.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import Services from '../components/services/Services';
+import ServicesForm from '../components/services/ServicesForm';
+import { connect } from 'react-redux';
+import { fetchServices } from '../actions/fetchServices';
+import styled from '@emotion/styled'
+
+const DisplayDiv = styled.div`
+    background-color: #4297A0;
+    text-align: center;
+    display: flex;
+
+    .form {
+        background-color: #F4EAE6;
+        width: 60%;
+        margin: 2%;
+        padding: 5%;
+    }
+
+    .display {
+        color: #F4EAE6;
+        text-align: left;
+        padding: 2%;
+        width: 30%;
+        margin: 5%;
+        td {
+            width: 150px;
+        }
+    }
+`
+class ServicesContainer extends Component {
+    
+    componentDidMount() {
+        this.props.fetchServices()
+    }
+    
+    render() {
+        return (
+            <DisplayDiv className="container">
+                <ServicesForm/>
+                <Services services={this.props.services} />
+            </DisplayDiv>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        services: state.services
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        fetchServices: () => {
+            dispatch(fetchServices())
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ServicesContainer);
