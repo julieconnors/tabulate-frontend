0198c2c8b5eaa01ea68da79e8cb8cf345f289a7a
julieconnors <juliana.g.connors@gmail.com>
2021-06-05 19:29:58 -0400

    move Navbar and say sheet/statement component routing

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import HorseContainer from './HorseContainer';
 import ServicesContainer from './ServicesContainer';
+import { BrowserRouter as Router, Route } from 'react-router-dom'; 
+import NavBar from './components/NavBar'
+import DaySheetContainer from './containers/DaySheetContainer';
+import StatementContainer from './containers/StatementContainer'
+import Home from './containers/Home'
 // import ErrorBoundary from '../components/ErrorBoundary'
 
 const Home = () => (
     <div>
+        <Router>
+            <NavBar />
+            {/* <Route exact path="/" component={Home}/> */}
+            <Route exact path="/day-sheet" component={DaySheetContainer} />
+            <Route exact path="/statements" component={StatementContainer} />
+        </Router>
         {/* <ErrorBoundary> */}
             <HorseContainer />
             <ServicesContainer />
