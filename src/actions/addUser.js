a1a462360f39ff3fe78701ab027d5f61cca1e8ec
julieconnors <juliana.g.connors@gmail.com>
2021-06-08 21:11:45 -0400

    add fetch request to authenticate user

---
+++ src/actions/addUser.js
@@ -0,0 +1,18 @@
+export function addUser(data) {
+    return (dispatch) => {
+        fetch('http://localhost:3001/api/v1/users', {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }, 
+            method: 'POST',
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then(user => {
+            dispatch({ type: 'ADD_USER', payload: user })
+            // sessionStorage.setItem('token', user.jwt)
+            // return user
+        })
+    }
+}
