9458ed43bc93f54363b6048f9188e7d9c1c0bcb1
julieconnors <juliana.g.connors@gmail.com>
2021-06-09 20:36:53 -0400

    include variables to pass props to children

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,25 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import HorseContainer from './HorseContainer';
 import ServicesContainer from './ServicesContainer';
-import { BrowserRouter as Router, Route } from 'react-router-dom'; 
-import NavBar from '../components/NavBar'
-import DaySheetContainer from './DaySheetContainer';
-import StatementContainer from './StatementContainer'
-// import ErrorBoundary from '../components/ErrorBoundary'
 
-const Home = () => (
-    <div>
-        <Router>
-            <NavBar />
-            <Route exact path="/account" component={Home}/>
-            <Route exact path="/day-sheet" component={DaySheetContainer} />
-            <Route exact path="/statements" component={StatementContainer} />
-        </Router>
-        {/* <ErrorBoundary> */}
-            <HorseContainer />
-            <ServicesContainer />
-        {/* </ErrorBoundary> */}
-    </div>
-)
+class Home extends Component {
+    render(){
+        const currentUser = this.props.currentUser.data.attributes
+        const horses = currentUser.horses
+        const services = currentUser.services
 
+        return(
+            <div>
+                <HorseContainer horses={horses} />
+                <ServicesContainer services={services}/>
+            </div>
+        )
+    }
+}
+    
 export default Home;
