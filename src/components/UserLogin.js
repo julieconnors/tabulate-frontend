ab19b12b3c3ec1f7f55e02b0fd9d24d4d43f86a8
julieconnors <juliana.g.connors@gmail.com>
2021-06-24 06:54:34 -0400

    remove event handling

--- src/components/UserLogin.js
+++ src/components/UserLogin.js
@@ -1,33 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
-import { connect } from 'react-redux'
-import { authenticateUser } from '../actions/authenticateUser';
 import '../App.css'
-
-
 class UserLogin extends Component {
-
-    // state = {
-    //     username: "",
-    //     password: ""
-    // }
-
-    // handleChange = (event) => {
-    //     this.setState({
-    //         [event.target.name]: event.target.value.trim()
-    //     })
-    // }
-
-    // handleSubmit = (event) => {
-    //     event.preventDefault()
-    //     this.props.authenticateUser(this.state)
-    //     debugger
-    //     this.props.returnUser(this.state)
-    //     this.setState({
-    //         username: "",
-    //         password: ""
-    //     })
-    // }
 
     render() {
         return (
@@ -47,4 +21,4 @@ class UserLogin extends Component {
     }
 }
 
-export default connect(null, { authenticateUser })(UserLogin);
+export default UserLogin;
